0534f850b88c6e632c4262250ae3d4f882f737cd
GraSo <santigs211@gmail.com>
2026-05-23 18:11:29 -0300

    feat: initialize frontend project with Next.js, TypeScript, and Tailwind CSS
    - Add package.json with dependencies for Next.js, React, and Tailwind CSS
    - Create postcss.config.mjs for Tailwind CSS integration
    - Add SVG assets for UI components
    - Set up TypeScript configuration with strict settings and module resolution

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  
+      <main className="flex w-full  flex-col items-center  py-32 px-16   ">
+        <h1 className="text-6xl  font-bold tracking-tight  text-gray-900 dark:text-white ">
+          Welcome to{" "}
+          <span className="text-blue-500">Smart Check Automation</span>
+        </h1>
+      </main>
+  
+  );
+}
